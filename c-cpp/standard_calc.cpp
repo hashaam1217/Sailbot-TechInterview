fe39d3578cb3668f5ae20668b1d965f3961a4fa2
hashaam1217 <hashaam1217@gmail.com>
2023-01-14 22:43:00 -0800

    Answered technical quiz
    Added solutions to the two unanswered functions.

--- c-cpp/standard_calc.cpp
+++ c-cpp/standard_calc.cpp
@@ -1,5 +1,9 @@
 #include "stdbool.h"
 #include "standard_calc.h"
+#include <cmath>
+
+using namespace std;
+
 
 /**
  * @brief Bounds the provided angle between [-180, 180) degrees.
@@ -12,8 +16,38 @@
  *
  * @return float: The bounded angle in degrees.
  */
-float bound_to_180(float angle) {
-    return 0;
+float bound_to_180(float angle) 
+{
+    float output;
+    float revolutions;
+
+    //Converts to 360 degrees
+    revolutions = angle / 360;
+    revolutions = int(revolutions);
+    if (revolutions != 0)
+    {
+      output = angle - (360*revolutions);
+    }
+
+    else
+    {
+      output = angle;
+    }
+
+    //Converts 360 scale to a -180 to 180 scale
+    if (output >= 180)
+    {
+      output = output - 360 ; 
+    }
+
+    else if (output < -180)
+    {
+      output = 360 + output; 
+    }
+  
+  
+
+    return std::round(output * 100) / 100.0;
 }
 
 /**
@@ -28,6 +62,40 @@ float bound_to_180(float angle) {
  * @param second_angle: The second bounding angle in degrees.
  * @return bool: TRUE when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, FALSE otherwise
  */
-bool is_angle_between(float first_angle, float middle_angle, float second_angle) {
-    return true;
+bool is_angle_between(float first_angle, float middle_angle, float second_angle) 
+{
+    float first, second;
+    bool result;
+  
+    //Edge case (second angle > first angle)
+    if (first_angle < second_angle)
+    {
+      first = second_angle;
+      second = first_angle; 
+    }
+
+    else
+    {
+      first = first_angle;
+      second = second_angle;
+    }
+  
+    //Normal case (first angle > second angle)
+    if (first >= middle_angle && second <= middle_angle)
+    {
+      result = true;
+    }
+
+    else
+    {
+      result = false;  
+    }
+
+    //Checks for reflex angle 
+    if (first - second > 180)
+    {
+      result = !result;
+    }
+
+    return result;
 }
